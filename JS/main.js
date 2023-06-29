b96eded884a7198eb8232f96a0168c9617734a34
ojtineo <ojtineo@gmail.com>
2023-06-29 03:55:20 -0300

    primer commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(function (item) {
+
+    item.addEventListener("click", function (e) {
+        const currentItem = document.querySelector(".active");
+
+        currentItem.classList.remove("active");
+
+        e.target.classList.add("active")
+    });
+});
+
